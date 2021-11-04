acd48a6d5ec8861444ab44624121ec5a5804db43
universitywits1 <85569369+universitywits1@users.noreply.github.com>
2021-11-04 08:17:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -73,9 +73,9 @@ async def start(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention), the
+            caption=script.START_TXT.format(message.from_user.mention), 
             reply_markup=reply_markup,
-            parse_mode='html'
+            parse_mode='html' 
         )
         if not await db.is_user_exist(message.from_user.id):
             await db.add_user(message.from_user.id, message.from_user.first_name)
