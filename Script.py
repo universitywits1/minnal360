ae10ae04220dc485f9ce3dee1cc39e1bbc69bd28
universitywits1 <85569369+universitywits1@users.noreply.github.com>
2021-11-04 07:44:25 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,8 +3,8 @@ class script(object):
 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href='https://t.me/Po_njibot'>KeshavaN Ponjikkara</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """<b>✯ 𝙼𝚈 𝙽𝙰𝙼𝙴:</b> മിന്നൽ മുരളി 
-<b>✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁:</b> <a href=https://t.me/IET_OWNER>IQBAL</a>
+    ABOUT_TXT = """<b>✯ 𝙼𝚈 𝙽𝙰𝙼𝙴:</b> Keshavan Ponjikkara
+<b>✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁:</b> <a href=https://t.me/Movies360help/68>Keyzer Soze</a>
 <b>✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈:</b> 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 <b>✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴:</b> 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 <b>✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴:</b> 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
